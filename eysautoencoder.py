f2662f6a480e4df01d1e5238d51a8a32798fdf2f
educaysoft <educaysoft@gmail.com>
2019-02-21 21:48:10 -0500

    Create eysautoencoder.py

---
+++ eysautoencoder.py
@@ -0,0 +1,9 @@
+from IPython.display import Image, SVG
+import matplotlib.pyplot as plt
+import numpy as np
+import keras
+from keras.datasets import mnist
+from keras.models import Model, Sequential
+from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D, Flatten, Reshape
+from keras import regularizers
+
